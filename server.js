ce421caf3f4de76657019b682e814b50bf79997a
Ruben <rubensteen123@gmail.com>
2023-03-06 15:13:01 +0000

    Added phidget connection

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const host = '0.0.0.0';
 const port = 3000;
 const app = express();
 
+const phidgetServerDisable = process.env.PHIDGET_SERVER_DISABLE || false;
+const phidgetServerIP = process.env.PHIDGET_SERVER_IP || "localhost";
+const phidgetServerPort = process.env.PHIDGET_SERVER_PORT || 5661;
+const phidgetServerName = process.env.PHIDGET_SERVER_NAME || "";
+const phidgetServerPassword = process.env.PHIDGET_SERVER_PASSWORD || "";
+
 // serve static assets normally
 app.use(express.static(__dirname + '/app/dist'));
+
+app.get('/foobar', (request, response) => {
+  response.send('hello world')
+})
 
 // handle every other route with index.html, which will contain
 // a script tag to your application's JavaScript file(s).
@@ -14,4 +25,23 @@ app.get('*', function (request, response) {
 });
 
 app.listen(port, host);
-console.log("server started on port " + port);
+console.log("Node server started on " + process.env.APP_PORT);
+
+var phidget22 = require('phidget22');
+
+var conn = new phidget22.Connection({
+	hostname: phidgetServerIP,
+	port: phidgetServerPort,
+	name: phidgetServerName,
+	passwd: phidgetServerPassword,
+	onAuthenticationNeeded: function() { return "password"; },
+	onError: function(code, msg) { console.error("Connection Error:", msg); },
+	onConnect: function() { console.log("Connected to the phidget server on " + phidgetServerIP + ":" + phidgetServerPort); },
+	onDisconnect: function() { console.log("Disconnected"); }
+});
+
+if (!phidgetServerDisable) {
+	conn.connect().catch(function(err) {
+		console.error(err);
+	});
+}
