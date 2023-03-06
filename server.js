3b5eec68e968badbbe3076d3130995c63fb2ca4c
Ruben <rubensteen123@gmail.com>
2023-03-06 13:59:05 +0000

    boiler

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const host = '0.0.0.0';
+const port = 3000;
+const app = express();
+
+// serve static assets normally
+app.use(express.static(__dirname + '/app/dist'));
+
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+app.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.listen(port, host);
+console.log("server started on port " + port);
